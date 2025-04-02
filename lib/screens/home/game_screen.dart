b29153327b03d3da82adaf67c0e0037ba2d9c0ec
JGallJ <162924316+JGallJ@users.noreply.github.com>
2025-04-02 22:59:47 +0200

    First look at shop widget

--- lib/screens/home/game_screen.dart
+++ lib/screens/home/game_screen.dart
@@ -6,6 +6,7 @@ import 'package:nogler/widgets/joker_cards_widget.dart';
 import 'package:nogler/widgets/main_cards_widget.dart';
 import 'package:nogler/widgets/selected_cards_widget.dart';
 import 'package:nogler/widgets/setting_button_widget.dart';
+import 'package:nogler/widgets/shop_fase/shop_widget.dart';
 import 'package:nogler/widgets/sidebar_widget.dart';
 import 'package:nogler/widgets/timer_widget.dart';
 
@@ -21,9 +22,13 @@ class GameScreenState extends State<GameScreen> {
   final GlobalKey<MainCardsState> _mainCardsKey = GlobalKey();
   final GlobalKey<SelectedCardsState> _selectedCardsKey = GlobalKey();
 
-  // Show game fase widgets
-  bool _showGameFaseWidgets = true;
-  bool _hidden = false;
+  // Variables to animate the exit of the elements off screen
+  bool _animateShowGameFaseWidgets = true;
+  bool _animateShowShopFaseWidgets = false;
+
+  // Show game fase widgets visibly
+  bool _showGameFaseWidget = true;
+  bool _showShopFaseWidget = false;
 
   int _remainingCards = 0;
   @override
@@ -37,102 +42,120 @@ class GameScreenState extends State<GameScreen> {
               Row(
                 children: [
                   Sidebar(), // Sidebar for navigation and game info
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 5),
 
-                        // Section for the Joker cards at the top left
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20),
-                          child: Row(
-                            mainAxisAlignment:
-                                MainAxisAlignment
-                                    .start, // Aligns JokerCards to the left
-                            children: [JokerCards()],
-                          ),
-                        ),
+                  _showGameFaseWidget
+                      ? Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 5),
 
-                        // Move down the widget to change fase
-                        Visibility(
-                          visible: _hidden,
-                          child: AnimatedSlide(
-                            offset:
-                                _showGameFaseWidgets
-                                    ? Offset(0, 0)
-                                    : Offset(0, 3),
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                            child: SelectedCards(key: _selectedCardsKey),
-                          ),
-                        ), // Widget displaying selected cards
-
-                        const SizedBox(height: 5),
-                        // Move down the widget to change fase
-                        Visibility(
-                          visible: _hidden,
-                          child: AnimatedSlide(
-                            offset:
-                                _showGameFaseWidgets
-                                    ? Offset(0, 0)
-                                    : Offset(0, 3),
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                            // MainCards widget in the center
-                            child: MainCards(
-                              key: _mainCardsKey,
-                              onDeckUpdated: (value) {
-                                WidgetsBinding.instance.addPostFrameCallback((
-                                  _,
-                                ) {
-                                  setState(() {
-                                    _remainingCards = value;
-                                  });
-                                });
-                              },
-                              onPlayCards: (playedCards) {
-                                _selectedCardsKey.currentState?.showCards(
-                                  playedCards,
-                                );
-                              },
+                            // Section for the Joker cards at the top left
+                            Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment
+                                        .start, // Aligns JokerCards to the left
+                                children: [JokerCards()],
+                              ),
                             ),
-                          ),
-                        ),
 
-                        // Action buttons and deck info at the bottom
-                        Visibility(
-                          visible: _hidden,
-                          child: AnimatedSlide(
-                            offset:
-                                _showGameFaseWidgets
-                                    ? Offset(0, 0)
-                                    : Offset(0, 3),
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(width: 75),
-                                ActionButtons(
-                                  onDiscard: () {
-                                    _mainCardsKey.currentState
-                                        ?.discardSelectedCards();
+                            // Move down the widget to change fase
+                            Visibility(
+                              visible: _showGameFaseWidget,
+                              child: AnimatedSlide(
+                                offset:
+                                    _animateShowGameFaseWidgets
+                                        ? Offset(0, 0)
+                                        : Offset(0, 3),
+                                duration: Duration(milliseconds: 300),
+                                curve: Curves.easeInOut,
+                                child: SelectedCards(key: _selectedCardsKey),
+                              ),
+                            ), // Widget displaying selected cards
+
+                            const SizedBox(height: 5),
+                            // Move down the widget to change fase
+                            Visibility(
+                              visible: _showGameFaseWidget,
+                              child: AnimatedSlide(
+                                offset:
+                                    _animateShowGameFaseWidgets
+                                        ? Offset(0, 0)
+                                        : Offset(0, 3),
+                                duration: Duration(milliseconds: 300),
+                                curve: Curves.easeInOut,
+                                // MainCards widget in the center
+                                child: MainCards(
+                                  key: _mainCardsKey,
+                                  onDeckUpdated: (value) {
+                                    WidgetsBinding.instance
+                                        .addPostFrameCallback((_) {
+                                          setState(() {
+                                            _remainingCards = value;
+                                          });
+                                        });
                                   },
-                                  onPlayHand: () {
-                                    _mainCardsKey.currentState
-                                        ?.playSelectedCards();
+                                  onPlayCards: (playedCards) {
+                                    _selectedCardsKey.currentState?.showCards(
+                                      playedCards,
+                                    );
                                   },
                                 ),
-                                SizedBox(width: 15),
-                                DeckInfo(remainingCards: _remainingCards),
-                              ],
+                              ),
                             ),
-                          ),
+
+                            // Action buttons and deck info at the bottom
+                            Visibility(
+                              visible: _showGameFaseWidget,
+                              child: AnimatedSlide(
+                                offset:
+                                    _animateShowGameFaseWidgets
+                                        ? Offset(0, 0)
+                                        : Offset(0, 3),
+                                duration: Duration(milliseconds: 300),
+                                curve: Curves.easeInOut,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(width: 75),
+                                    ActionButtons(
+                                      onDiscard: () {
+                                        _mainCardsKey.currentState
+                                            ?.discardSelectedCards();
+                                      },
+                                      onPlayHand: () {
+                                        _mainCardsKey.currentState
+                                            ?.playSelectedCards();
+                                      },
+                                    ),
+                                    SizedBox(width: 15),
+                                    DeckInfo(remainingCards: _remainingCards),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
+                      )
+                      : Expanded(
+                        child: Column(
+                          children: [
+                            // Section for the Joker cards at the top left
+                            Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment
+                                        .start, // Aligns JokerCards to the left
+                                children: [JokerCards()],
+                              ),
+                            ),
+                            Shop(),
+                          ],
+                        ),
+                      ),
                 ],
               ),
 
@@ -141,22 +164,31 @@ class GameScreenState extends State<GameScreen> {
               SettingsButton(
                 //TODO, puesto para cambiar entre fase de juego y tienda, cambiar a posteriori
                 onPressed: () {
-                  if (_showGameFaseWidgets) {
+                  // Animate Game Fase Widgets
+                  if (_animateShowGameFaseWidgets) {
                     setState(() {
-                      _showGameFaseWidgets = !_showGameFaseWidgets;
+                      // Init animation
+                      _animateShowGameFaseWidgets =
+                          !_animateShowGameFaseWidgets;
                     });
                     Future.delayed(Duration(milliseconds: 300), () {
                       setState(() {
-                        _hidden = !_hidden;
+                        // Change visible state of widgets
+                        _showGameFaseWidget = !_showGameFaseWidget;
+                        _showShopFaseWidget = !_showShopFaseWidget;
                       });
                     });
                   } else {
                     setState(() {
-                      _hidden = !_hidden;
+                      // Change visible state of widgets
+                      _showGameFaseWidget = !_showGameFaseWidget;
+                      _showShopFaseWidget = !_showShopFaseWidget;
                     });
                     Future.delayed(Duration(milliseconds: 10), () {
                       setState(() {
-                        _showGameFaseWidgets = !_showGameFaseWidgets;
+                        // Init animation
+                        _animateShowGameFaseWidgets =
+                            !_animateShowGameFaseWidgets;
                       });
                     });
                   }
